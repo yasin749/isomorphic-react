6701c6c50489421f2bbe70ea68b926f973cb6e9a
Yasin UYSAL <yasin.uysal@hepsiburada.com>
2020-05-28 10:21:51 +0300

    Mock data failover

--- src/universal/services/blog/blogService.js
+++ src/universal/services/blog/blogService.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
+// import blogs from './blogs';
+
 function getAllBlogs() {
     return axios.get('https://jsonplaceholder.typicode.com/posts');
+    /* Failover
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(blogs);
+        }, 300);
+    })*/
 }
 
 export {
